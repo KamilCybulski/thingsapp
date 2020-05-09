03454f8206b06beaaa102b87dacad5337b22cf25
Kamil <kamillcybulski@gmail.com>
2020-05-09 20:08:22 +0200

    Add svg support (#1)

--- metro.config.js
+++ metro.config.js
@@ -5,13 +5,25 @@
  * @format
  */
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-};
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const { resolver } = await getDefaultConfig();
+  const { sourceExts, assetExts } = resolver;
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
