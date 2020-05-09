e2a49b78e261a0cce244978f36cbd229100cfda2
zuy_szatan <kamillcybulski@gmail.com>
2020-05-09 16:25:59 +0200

    Add svg support

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,25 @@
 import React, { useCallback } from 'react';
 import styled from 'styled-components/native';
-import { Button } from 'react-native';
 import { SCREENS } from '.';
+import PlusIcon from '../icons/plus.svg';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 const HomeScreenContanier = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
   background-color: #006b3c;
+`;
+
+const AddProductButtonContent = styled.View`
+  width: 200px;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: lightblue;
+  padding: 0 10px;
 `;
 
 const HomeScreenText = styled.Text`
@@ -22,7 +34,12 @@ export const HomeScreen = ({ navigation }) => {
   return (
     <HomeScreenContanier>
       <HomeScreenText>Welcome to HomeScreen</HomeScreenText>
-      <Button title="Add new product" onPress={handlePress} />
+      <TouchableHighlight onPress={handlePress}>
+        <AddProductButtonContent onPress={handlePress}>
+          <PlusIcon width={20} height={20} fill="#fff" />
+          <HomeScreenText>Add new product</HomeScreenText>
+        </AddProductButtonContent>
+      </TouchableHighlight>
     </HomeScreenContanier>
   );
 };
