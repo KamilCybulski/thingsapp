8e0edf134f04c8153b6ce86e3c51d4e4a629c0a7
zuy_szatan <kamillcybulski@gmail.com>
2020-05-26 22:59:41 +0200

    Add creating storages

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-exports.createUser = functions.auth.user().onCreate(user => {
+const createUser = user => {
   const { phoneNumber, uid } = user;
 
   return admin
@@ -12,6 +12,37 @@ exports.createUser = functions.auth.user().onCreate(user => {
     .doc(uid)
     .set({
       phoneNumber,
-      fridges: [],
+      ownStorages: [],
+      accessibleStorages: [],
     });
-});
+};
+
+const createStorage = async (data, context) => {
+  // const { uid } = context.auth;
+  const uid = 'zenon';
+
+  const storageRef = await admin
+    .firestore()
+    .collection('storages')
+    .add({
+      owner: uid,
+      participants: [],
+      name: data,
+      items: [],
+    });
+
+  await admin
+    .firestore()
+    .collection('users')
+    .doc(uid)
+    .update(
+      'ownStorages',
+      admin.firestore.FieldValue.arrayUnion(storageRef.id),
+    );
+
+  const result = await storageRef.get();
+  return result.data();
+};
+
+exports.createUser = functions.auth.user().onCreate(createUser);
+exports.createStorage = functions.https.onCall(createStorage);
