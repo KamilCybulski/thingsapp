17be9709a46888e78170381ee8151eb992a8a0ed
zuy_szatan <kamillcybulski@gmail.com>
2020-05-29 00:06:50 +0200

    Add storage screen

---
+++ src/screens/StorageScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { useSelector } from 'react-redux';
+
+const StorageScreenContainer = styled.View`
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+`;
+
+const StorageScreenHeader = styled.Text`
+  color: #000;
+`;
+
+const StorageScreen = ({ route }) => {
+  const { storageId } = route.params;
+  const storage = useSelector(state =>
+    state.storages.own.find(item => item.id === storageId),
+  );
+
+  console.log('PICKED STORAGE: ', storage);
+
+  return (
+    <StorageScreenContainer>
+      <StorageScreenHeader>This is storage screen</StorageScreenHeader>
+    </StorageScreenContainer>
+  );
+};
+
+export default StorageScreen;
