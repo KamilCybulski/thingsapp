e13e8305f76635437606f08e1bc24432960226c1
zuy_szatan <kamillcybulski@gmail.com>
2020-05-31 22:38:01 +0200

    remove button from home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,6 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import styled from 'styled-components/native';
 
-import { SCREENS } from '.';
-import { IconButton } from '../components';
-import AddIcon from '../assets/icons/add--white.svg';
 import { StoragesList } from '../components/';
 
 const HomeScreenContanier = styled.View`
@@ -16,20 +13,11 @@ const HomeScreenText = styled.Text`
   color: #fff;
 `;
 
-const HomeScreen = ({ navigation }) => {
-  const handlePress = useCallback(() => {
-    navigation.navigate(SCREENS.addThing);
-  }, [navigation]);
-
-  return (
-    <HomeScreenContanier>
-      <HomeScreenText>Welcome to HomeScreen</HomeScreenText>
-      <StoragesList />
-      <IconButton onPress={handlePress}>
-        <AddIcon width={30} height={30} fill="#fff" />
-      </IconButton>
-    </HomeScreenContanier>
-  );
-};
+const HomeScreen = ({ navigation }) => (
+  <HomeScreenContanier>
+    <HomeScreenText>Welcome to HomeScreen</HomeScreenText>
+    <StoragesList />
+  </HomeScreenContanier>
+);
 
 export default HomeScreen;
